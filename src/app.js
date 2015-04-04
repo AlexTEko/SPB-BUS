3f5fa6944311ae0c8e244758aa7b2a4641b7380c
Alexander Teseyko <alexteko@gmail.com>
2015-04-04 12:31:57 +0300

    Project reorganization

--- src/app.js
+++ src/app.js
@@ -1,11 +1,88 @@
 var UI = require('ui');
-var ajax = require('ajax');
 var Vibe = require('ui/vibe');
-
+var ajax = require('ajax');
+//===============================================CONFIG=================================================================
 var user_id = "100";
 //var api = 'http://spbbus.pebblenow.ru/api_test.php'; //test api
 var api = 'http://spbbus.pebblenow.ru/api.php'; //work api
 
+var locationOptions = {
+  enableHighAccuracy: true, 
+  maximumAge: 5000, 
+  timeout: 10000
+};
+//======================================================================================================================
+//===================================================UI=================================================================
+var card = new UI.Card({
+  title:'Wait',
+  subtitle:'Working...'
+});
+
+var gpscard = new UI.Card({
+  title:'Wait',
+  subtitle:'Finding your location...'
+});
+
+var menuItems = [
+  {
+    title: "Go",
+    subtitle: "Find nearest stops!"
+  },
+];
+
+var mainMenu = new UI.Menu({
+  sections: [{
+    title: 'SPB BUS',
+    items: menuItems
+  },{
+    title: 'Favorite stops' 
+  }
+]
+});
+
+mainMenu.on('select', function(e) {
+  console.log('Select:' + e.itemIndex);
+  switch(e.itemIndex) {
+      case 0:
+          gpscard.show();
+          navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+          break;
+      case 1:
+//           var key = 5;
+//           var value = 'Some string';
+
+//           localStorage.setItem(key, value);
+//           var value = localStorage.getItem(key);
+          
+          break;
+  }
+});
+
+mainMenu.on('longSelect', function(e) {
+  console.log('longSelect:' + e.itemIndex);
+});
+
+
+// var favoriteIds = [22,33,44];
+// localStorage.setItem(0, favoriteIds);
+
+// localStorage.setItem(22,  JSON.stringify({id:22, title:"22", subtitle:"Торжковский рынок"}));
+// //localStorage.setItem(33, { id:22, title:"22", subtitle:"Новосибирская"});
+// //localStorage.setItem(44, { id:22, title:"22", subtitle:"Черная Речка"});
+
+// var favoriteItems = [];
+// for (var i = 0; i<favoriteIds.length; i++) {
+//   favoriteItems.push(JSON.parse(localStorage.getItem(favoriteIds[i])));
+//   console.log(JSON.parse(localStorage.getItem(favoriteIds[i])));
+// }
+
+// mainMenu.items(1,  favoriteItems);
+mainMenu.show();
+//Accel.init();
+//======================================================================================================================
+//===============================================LOGIC==================================================================
+
+//Make array of Transport Items
 var parseTrans = function(data, quantity) {
   var items = [];
   for(var i = 0; i < quantity; i++) {
@@ -21,6 +98,7 @@ var parseTrans = function(data, quantity) {
   return items;
 };
 
+//Make array of Stops Items
 var parseStops = function(data, quantity) {
   var items = [];
   for(var i = 0; i < quantity; i++) {
@@ -38,16 +116,6 @@ var parseStops = function(data, quantity) {
   }
   return items;
 };
-
-var card = new UI.Card({
-  title:'Wait',
-  subtitle:'Working...'
-});
-
-var gpscard = new UI.Card({
-  title:'Wait',
-  subtitle:'Finding your location...'
-});
 
 // Get nearest stops near lat, lon coordinates
 function get_nearest_stops(lat, lon) {
@@ -80,7 +148,7 @@ function get_nearest_stops(lat, lon) {
     }
   );
 }
-
+//Get Stop Transport forecast
 function get_stop(stopid) {    
         card.show();
         ajax(
@@ -106,47 +174,15 @@ function get_stop(stopid) {
         }
       );
 }
-var locationOptions = {
-  enableHighAccuracy: true, 
-  maximumAge: 5000, 
-  timeout: 10000
-};
-
+//Get coordinates successfully
 function locationSuccess(pos) {
   gpscard.hide();
   get_nearest_stops(pos.coords.latitude, pos.coords.longitude);
   //console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude + ' accuracy= ' + pos.coords.accuracy );
 }
-
+//Get coordinates failed
 function locationError(err) {
   gpscard.hide();
   //console.log('location error (' + err.code + '): ' + err.message);
 }
-
-var menuItems = [
-  {
-    title: "Go",
-    subtitle: "Find stops!"
-  },
-];
-
-var mainMenu = new UI.Menu({
-  sections: [{
-    title: 'SPB BUS',
-    items: menuItems
-  }]
-});
-
-mainMenu.on('select', function(e) {
-  switch(e.itemIndex) {
-      case 0:
-          
-          break;
-  }
-});
-
-//mainMenu.show();
-//Accel.init();
-
-gpscard.show();
-navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+//======================================================================================================================

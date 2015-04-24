18f71ba45175a154ef31c9f25ab82fc1fa7cbeb6
Alexander Teseyko <alexteko@gmail.com>
2015-04-24 11:47:52 +0300

    Optimizing and bugfixing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var UI = require('ui');
 var Vibe = require('ui/vibe');
 var ajax = require('ajax');
-//var Accel = require('ui/accel');
+var Settings = require('settings');
 //===============================================CONFIG=================================================================
 var user_id = "120";
 //var api = 'http://spbbus.pebblenow.ru/api_test.php'; //test api
@@ -9,7 +9,7 @@ var api = 'http://spbbus.pebblenow.ru/api.php'; //work api
 
 var locationOptions = {
   enableHighAccuracy: true, 
-  maximumAge: 5000, 
+  maximumAge: 0, 
   timeout: 10000
 };
 //======================================================================================================================
@@ -31,23 +31,22 @@ var mainMenu = new UI.Menu({
 ]
 });
 
+mainMenu.on('click','back',function() {
+  mainMenu.hide();
+});
+
 var updateFavorites = function() {
   var favoriteItems = [];
-  //console.log("Length = " + favoriteIds.length);
   var favoriteIds = JSON.parse(localStorage.getItem("favoriteIds"));
   if (favoriteIds !== null) {
     for (var i = 0; i<favoriteIds.length; i++) {
       favoriteItems.push(JSON.parse(localStorage.getItem(favoriteIds[i])));
-      //console.log("Item #" + favoriteIds[i] + " is: " + JSON.parse(localStorage.getItem(favoriteIds[i])));
-     // console.log(favoriteItems[favoriteIds[i]]);
     }
   }
   mainMenu.items(1,  favoriteItems);
 };
 
 var card = new UI.Card({
- // title:'Wait',
- // subtitle:'Working...'
   banner: 'images/update.png'
 });
 
@@ -56,6 +55,11 @@ var addtof = new UI.Card({
 });
 
 addtof.on('click', 'back', function() {
+  updateFavorites();
+  addtof.hide();
+});
+
+addtof.on('click', 'select', function() {
   updateFavorites();
   addtof.hide();
 });
@@ -69,30 +73,28 @@ remfromf.on('click', 'back', function() {
   remfromf.hide();
 });
 
+remfromf.on('click', 'select', function() {
+  updateFavorites();
+  remfromf.hide();
+});
+
 var gpscard = new UI.Card({
-  //title:'Wait',
-  //subtitle:'Finding your location...'
   banner: 'images/location.png'
 });
 
 mainMenu.on('select', function(e) {
-  //console.log('Select item:' + e.itemIndex);
-  //console.log('Select section:' + e.sectionIndex);
   if (e.sectionIndex === 0) {
           gpscard.show();
           navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
   }
   else {
-    //console.log('Stop item id:' + e.item.id);
-      var stopid = e.item.id;
       card.show();
-      get_stop(stopid);
+      get_stop(e.item.id);
   }
 });
 
 mainMenu.on('longSelect', function(e) {
   if (e.sectionIndex === 1) {
-    //console.log('Remove item stop:' + e.item.id);
     var favoriteIds = JSON.parse(localStorage.getItem("favoriteIds"));
     if (favoriteIds !== null) {
       var index = favoriteIds.indexOf(e.item.id);
@@ -106,9 +108,55 @@ mainMenu.on('longSelect', function(e) {
   }
 });
 
+var menuStops = new UI.Menu({
+  sections: [{
+    title: 'Nearest stops',
+    items: []
+  }]
+});   
+
+menuStops.on('select', function(e) {
+  card.show();
+  get_stop(e.item.id);
+});
+
+menuStops.on('click','back',function() {
+  menuStops.hide();
+});
+
+menuStops.on('longSelect', function(e) {
+  var favoriteIds = JSON.parse(localStorage.getItem(("favoriteIds")));
+  if (favoriteIds !== null) {
+    var index = favoriteIds.indexOf(e.item.id);
+    if (index < 0)
+      favoriteIds.push(e.item.id);
+  }
+  else {
+    favoriteIds = [];
+    favoriteIds.push(e.item.id);
+  }
+  localStorage.setItem("favoriteIds", JSON.stringify(favoriteIds));
+  localStorage.setItem(e.item.id, JSON.stringify({id:e.item.id, title:e.item.title, subtitle:e.item.subtitle}));
+  addtof.show();
+});
+
+var menuTrans = new UI.Menu({
+  sections: [{
+    title: 'Arriving transport',
+    items: []
+  }]
+});
+
+menuTrans.on('click','back',function() {
+  menuTrans.hide();
+});
+
+menuTrans.on('longSelect', function(e) {
+  get_stop();
+});
+
 updateFavorites();
 mainMenu.show();
-//Accel.init();
 //======================================================================================================================
 //===============================================LOGIC==================================================================
 
@@ -135,8 +183,6 @@ var parseStops = function(data, quantity) {
     var id = data.stops[i].id;
     var name = data.stops[i].name;
     var type = data.stops[i].type;
-//    var lat = data.stops[i].lat;
-//    var lon = data.stops[i].lon;
     var distance = data.stops[i].distance;
     items.push({
       id:id,
@@ -156,94 +202,46 @@ function get_nearest_stops(lat, lon) {
       type:'json'
     },
     function(data) {
-      var itemsStops = parseStops(data, data.count);  
-      var menuStops = new UI.Menu({
-        sections: [{
-          title: 'Nearest stops',
-          items: itemsStops
-        }]
-      });    
+      menuStops.items(0,parseStops(data, data.count));
       menuStops.show();
       Vibe.vibrate('short');
       card.hide();
-      menuStops.on('select', function(e) {
-        var stopid = e.item.id;
-        card.show();
-        get_stop(stopid);
-      });
-      menuStops.on('longSelect', function(e) {
-        //var stopid = e.item.id;
-        var favoriteIds = JSON.parse(localStorage.getItem(("favoriteIds")));
-        if (favoriteIds !== null) {
-          var index = favoriteIds.indexOf(e.item.id);
-          if (index < 0)
-            favoriteIds.push(e.item.id);
-        }
-        else {
-          favoriteIds = [];
-          favoriteIds.push(e.item.id);
-        }
-        localStorage.setItem("favoriteIds", JSON.stringify(favoriteIds));
-        localStorage.setItem(e.item.id, JSON.stringify({id:e.item.id, title:e.item.title, subtitle:e.item.subtitle}));
-       // console.log("Save item " + e.item.id + ": "+ e.item.title + " " + e.item.subtitle);
-        addtof.show();
-      });
     },
     function(error) {
       card.hide();
-   //   console.log('Download failed: ' + error);
     }
   );
 }
 //Get Stop Transport forecast
-function get_stop(stopid) {    
-        card.show();
-        ajax(
-          {
-            url: api + '?stopid=' + stopid,
-            type:'json'
-          },
-          function(data) {
-            var itemsTrans = parseTrans(data, data.count);
-            var menuTrans = new UI.Menu({
-              sections: [{
-                title: 'Arriving transport',
-                items: itemsTrans
-              }]
-            });
-            menuTrans.on('longSelect', function(e) {
-              card.show();
-              ajax(
-                {
-                  url: api + '?stopid=' + stopid,
-                  type:'json'
-                },
-                function(data) {
-                  itemsTrans = parseTrans(data, data.count);
-                  menuTrans.items(0,itemsTrans);
-                  Vibe.vibrate('short');
-                  card.hide();
-                });
-            });
-            menuTrans.show();
-            card.hide();
-            Vibe.vibrate('short');
-        },
-        function(error) {
-          card.hide();
-     //     console.log('Download failed: ' + error);
-        }
-      );
+function get_stop(stopid) { 
+  if (stopid)
+    Settings.data('stopid', stopid);
+  else
+    stopid = Settings.data('stopid');
+    card.show();
+    ajax(
+      {
+        url: api + '?stopid=' + stopid + '&user_id=' + user_id,
+        type:'json'
+      },
+      function(data) {
+        menuTrans.items(0,parseTrans(data, data.count));
+        menuTrans.show();
+        card.hide();
+        Vibe.vibrate('short');
+      },
+      function(error) {
+        card.hide();
+      }
+    );
 }
 //Get coordinates successfully
 function locationSuccess(pos) {
   gpscard.hide();
   get_nearest_stops(pos.coords.latitude, pos.coords.longitude);
-  //console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude + ' accuracy= ' + pos.coords.accuracy );
 }
 //Get coordinates failed
 function locationError(err) {
   gpscard.hide();
-  //console.log('location error (' + err.code + '): ' + err.message);
 }
 //======================================================================================================================
